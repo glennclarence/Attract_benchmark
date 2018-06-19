fd510c765e653e8b568b37855c145620507b9b57
glennclarence <glenn.glashagen@gmail.com>
2018-06-19 16:04:53 +0200

    integrated the modes into to the result script

--- result.py
+++ result.py
@@ -17,19 +17,24 @@ def load_benchmarks( path_folder):
     for name_prot in os.listdir( path_folder ):
         dir_prot = os.path.join( path_folder, name_prot )
         benchmarks = [( d, os.path.join( dir_prot, d )) for d in os.listdir(dir_prot) if os.path.isdir( os.path.join( dir_prot,d)) and d != 'input' ]
-        bench.append( (name_prot,benchmarks))
+        input =[ os.path.join(dir_prot, d) for d in os.listdir(dir_prot) if os.path.isdir(os.path.join(dir_prot, d)) and d == 'input']
+        bench.append( (name_prot,input,benchmarks))
     return bench
 ext_rmsd = "-rmsd.result"
 ext_scored = "-sorted-dr.dat"
 filename_pdb = "-receptor-for-docking"
+
 def mode_ext( num_modes):
     return "-" + num_modes +"-modes.dat"
 
 bechmarks = load_benchmarks( path_folder )
 
-for name_protein, name_bench in bechmarks:
-
+for name_protein, input, name_bench in bechmarks:
+    print name_protein
     for name_singleBench in name_bench:
+        index_modes = 27
+        num_modes = name_singleBench[0][index_modes]
+        print "\t",name_singleBench[0]
         if name_singleBench[0] != 'input':
             filename_rmsd = os.path.join(name_singleBench[1],name_protein+filename_pdb+ext_rmsd)
             with open(filename_rmsd, 'r' ) as f:
@@ -42,10 +47,24 @@ for name_protein, name_bench in bechmarks:
 
             plt.plot( rmsd[:20], pos[:20])
             #plt.show()
-            print name_protein,name_singleBench[0],rmsd.mean(), rmsd[:10].mean(), rmsd[:50].mean(), np.sort(rmsd)[:10].mean(), np.sort(rmsd)[:50].mean()
+            print "\t\t", rmsd.mean(), rmsd[:10].mean(), rmsd[:50].mean(), np.sort(rmsd)[:10].mean(), np.sort(rmsd)[:50].mean()
 
-            index_modes = 26
-            num_modes = name_singleBench[0][index_modes]
-           # filename_modes  = os.path.join(name_singleBench[1],name_protein+filename_pdb+modes_ext(num_modes))
-            print filename_modes
+
+            if int(num_modes) > 0:
+                filename_modes  = os.path.join(input[0],name_protein+filename_pdb+mode_ext(num_modes))
+                with open(filename_modes, 'r') as f:
+                    lines = f.readlines()
+                    mode_idx = 1
+                    eigenvalues = np.zeros( int(num_modes), dtype = float)
+                    amplitude = np.zeros( int(num_modes) ,  dtype = float)
+                    for i, line in enumerate(lines):
+                        if len( line.split()) == 2 and int(line.split()[0]) == mode_idx:
+                            mode_idx += 1
+                            eigenvalues[mode_idx-2] = float(line.split()[1])
+                        elif len(line.split()) == 3:
+                            amplitude[mode_idx-2] += float(line.split()[0])*float(line.split()[0]) + float(line.split()[1])*float(line.split()[1]) + float(line.split()[2])*float(line.split()[2])
+                    print "\t\t", "amplitudes", amplitude
+                    print "\t\t", "eigenvalues", eigenvalues
+
+
 
